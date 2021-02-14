62aaa90bb7e3aa15042839adad9e99d981da518c
joannafg <joannafg@vt.edu>
2021-02-14 17:54:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import {Card, Row, Col, Space} from 'antd'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{marginTop: '25px'}}>
+      <Row gutter={40} align={"middle"} justify={"center"} type={"flex"} style={{marginTop: '25px'}}>
+        <Col span={6}><Card title="Card title">Card content</Card></Col>
+        <Col span={6}><Card title="Card title">Card content</Card></Col>
+        <Col span={6}><Card title="Card title">Card content</Card></Col>
+      </Row>
+    
+      <Row gutter={40} align={"middle"} justify={"center"} type={"flex"} style={{marginTop: '25px'}}>
+        <Col span={6}><Card title="Card title">Card content</Card></Col>
+        <Col span={6}><Card title="Card title">Card content</Card></Col>
+        <Col span={6}><Card title="Card title">Card content</Card></Col>
+      </Row>
     </div>
   );
 }
